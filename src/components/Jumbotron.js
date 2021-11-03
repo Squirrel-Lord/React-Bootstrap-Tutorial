8fb33db982d441e598ed1fa6edaa8d7ccfd82c08
Ross Baldwin <baldwinr17@gmail.com>
2021-11-03 17:49:28 -0500

    change Jumbotron

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Jumbotron as Jumbo, Container } from 'react-bootstrap';
 import styled from 'styled-components';
-import boatImage from '../assets/boatImage.jpg';
+import Image from '../assets/buddy_money.png';
 
 const Styles = styled.div`
   .jumbo {
-    background: url(${boatImage}) no-repeat fixed bottom;
+    background: url(${Image}) no-repeat fixed bottom;
     background-size: cover;
-    color: #efefef;
     height: 200px;
     position: relative;
     z-index: -2;
@@ -31,7 +30,6 @@ export const Jumbotron = () => (
       <div className="overlay"></div>
       <Container>
         <h1>Welcome</h1>
-        <p>Learn to code from my YouTube videos</p>
       </Container>
     </Jumbo>
   </Styles>
